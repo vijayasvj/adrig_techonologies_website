46f03338c46dc45447eeb55cc35526b7aa748855
Sreejith N Subramanian <nssreejith229@gmail.com>
2024-06-15 13:18:41 +0000

    fix: applied next-image robust practices

--- src/components/custom/homepage/ServiceCard.tsx
+++ src/components/custom/homepage/ServiceCard.tsx
@@ -9,7 +9,9 @@ type Props = {
 const ServiceCard = ({ service }: Props) => {
   return (
     <section className="hover:shadow-md hover:shadow-slate-200 hover:border-t hover:border-slate-200 w-full h-fit flex flex-col space-y-4 p-6 rounded-md transition-all duration-200">
-      <Image src={service.image} alt={service.title} />
+      <div className="h-full w-full relative">
+        <Image src={service.image} alt={service.title} fill className='object-contain'/>
+      </div>
       <span className="text-lg font-bold">{service.title}</span>
       <p className="leading-loose">{service.description}</p>
     </section>
