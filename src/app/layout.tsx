e9e52ebf14530beefffda052433412e51c91317d
Sreejith N Subramanian <nssreejith229@gmail.com>
2024-08-08 00:33:44 +0530

    Added matomo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import Script from "next/script"
 import Footer from "@/components/custom/Footer"
 import Navbar from "@/components/custom/Navbar"
 import ScheduleConversation from "@/components/custom/ScheduleConversation"
@@ -21,6 +22,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          id="matomo"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var _paq = _paq || [];
+              /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+              _paq.push(['trackPageView']);
+              _paq.push(['enableLinkTracking']);
+              (function() {
+                var u="//52.172.200.185/";
+                _paq.push(['setTrackerUrl', u+'piwik.php']);
+                _paq.push(['setSiteId', '1']);
+                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+              })();
+          `,
+          }}
+        />
+      </head>
       <body className={inter.className}>
         <div id="root">
           <Navbar />
